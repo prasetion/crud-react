b6d48a217a52693791f26c83e8c241d3590122ca
prasetion <prasetion@gmail.com>
2023-12-02 10:00:03 +0700

    adding protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,52 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useRoutes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import DetailPage from "./pages/DetailPage";
 import CreatePage from "./pages/CreatePage";
 import EditPage from "./pages/EditPage";
+import ProtectedRoute from "./hoc/ProtectedRoute";
+import AuthRoute from "./hoc/AuthRoute";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/create" element={<CreatePage />}></Route>
-        <Route path="/detail/:id" element={<DetailPage />}></Route>
-        <Route path="/edit/:id" element={<EditPage />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  const routes = [
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <HomePage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: "/login",
+      element: (
+        <AuthRoute>
+          <LoginPage />,
+        </AuthRoute>
+      ),
+    },
+    {
+      path: "/create",
+      element: (
+        <ProtectedRoute>
+          <CreatePage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: "/edit/:id",
+      element: (
+        <ProtectedRoute>
+          <EditPage />
+        </ProtectedRoute>
+      ),
+    },
+  ];
+
+  const element = useRoutes(routes);
+
+  return element;
 };
 
 export default App;
